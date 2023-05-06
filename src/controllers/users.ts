03c4edbcedd41fee13609f754643ae82db19163c
Pichborith Kong <pichborith@gmail.com>
2023-05-05 22:41:46 -0700

    create updateuser controller to update username

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,4 @@
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUserById, getUsers } from '../db/users';
 import { Request, Response } from 'express';
 
 export const getAllUsers = async (req: Request, res: Response) => {
@@ -19,6 +19,28 @@ export const deleteUser = async (req: Request, res: Response) => {
     const deletedUser = await deleteUserById(id);
 
     res.json(deletedUser);
+    return;
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+    return;
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const user = await getUserById(id);
+    user!.username = username;
+    await user?.save();
+    res.status(200).json(user).end();
     return;
   } catch (error) {
     console.log(error);
